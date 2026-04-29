9850f989801da2ed52a155b995537ddb33ae172a
tunderem <tunderem5@gmail.com>
2026-04-29 20:39:24 +0300

    подсказка

--- Assets/code/core.cs
+++ Assets/code/core.cs
@@ -1,14 +1,23 @@
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class core : MonoBehaviour
 {
-
-    
-    
-    
+    public int _a;
     void Start()
     {
-        var _variable = 0;
-        Debug.Log(_variable);
+       switch (_a)
+        {
+            case 0:
+                {
+                    Debug.Log(0);
+                    break;
+                }
+            case 1:
+                {
+                    Debug.Log(1);
+                    break;
+                }
+        }
     }
 }
